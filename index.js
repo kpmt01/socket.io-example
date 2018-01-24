ba48a52bd2b7731517927866cc40f963e9051acb
kpmt01 <kpmt01@gmail.com>
2018-01-24 13:15:45 +0300

    -
    -

--- index.js
+++ index.js
@@ -11,14 +11,34 @@ io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('chat message', function(msg){
     console.log(msg);
-    io.emit('chat message', msg);
+    if(msg[0] == '!'){
+      getCmd(msg);
+    }else{
+      io.emit('chat message', msg);
+    }
   });
   socket.on('file message', function(msg){
-    console.log('resim geldi.')
-    io.emit('chat message', 'Resim Yüklendi.');
+    console.log('picture requested.')
+    io.emit('chat message', 'image uplaoded');
     io.emit('file message', msg);
   });
 });
+
+
+function getCmd(cmd){
+  cmd = cmd.replace('!','');
+  if(cmd == 'clear'){
+    for(let i = 0;i < 30;i++){
+      io.emit('chat message', '------------------------------------');
+    }
+  }else if(cmd == 'whoiam'){
+    io.emit('chat message', 'you is awesome a human');
+  }else if(cmd == 'help' || cmd == 'cmd'){
+    io.emit('chat message', '!clear - !whoiam - !help - !cmd');
+  }else{
+    io.emit('chat message', cmd + ' is command not found');
+  }
+}
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
